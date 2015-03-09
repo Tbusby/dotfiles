5e5c52a8549ea90f51f268460352d60d711a005a
Tomas Busby <tomas.busby@gmail.com>
2015-03-09 12:20:28 +0000

    Fixed home/end keybinds to do the right thing

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -7,7 +7,9 @@
 bindkey "^K"      kill-whole-line                      # ctrl-k
 bindkey "^R"      history-incremental-search-backward  # ctrl-r
 bindkey "^A"      beginning-of-line                    # ctrl-a  
+bindkey "^[[H"      beginning-of-line                    # ctrl-a  
 bindkey "^E"      end-of-line                          # ctrl-e
+bindkey "^[[F"      end-of-line                          # ctrl-e
 bindkey "^[[B"      history-search-forward               # down arrow
 bindkey "^[[A"      history-search-backward              # up arrow
 bindkey "^D"      delete-char                          # ctrl-d
