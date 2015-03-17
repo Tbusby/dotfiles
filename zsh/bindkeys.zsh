318d414b8caa40e478813614410adc7ee66555fb
Tomas Busby <tomas.busby@gmail.com>
2015-03-17 11:49:45 +0000

    Fixed some keybinds for word skipping and the del key

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -7,12 +7,15 @@
 bindkey "^K"      kill-whole-line                      # ctrl-k
 bindkey "^R"      history-incremental-search-backward  # ctrl-r
 bindkey "^A"      beginning-of-line                    # ctrl-a  
-bindkey "^[[H"      beginning-of-line                    # ctrl-a  
+bindkey "^[[H"      beginning-of-line                    # home  
 bindkey "^E"      end-of-line                          # ctrl-e
-bindkey "^[[F"      end-of-line                          # ctrl-e
+bindkey "^[[F"      end-of-line                          # end
 bindkey "^[[B"      history-search-forward               # down arrow
 bindkey "^[[A"      history-search-backward              # up arrow
 bindkey "^D"      delete-char                          # ctrl-d
+bindkey "^[[3~"      delete-char                       # del
 bindkey "^F"      forward-char                         # ctrl-f
 bindkey "^B"      backward-char                        # ctrl-b
-#bindkey -v   # Default to standard vi bindings, regardless of editor string
+bindkey "^[^[[D"  backward-word                        # alt left arrow
+bindkey "^[^[[C"  forward-word                         # alt rigth arrow
+bindkey -v   # Default to standard vi bindings, regardless of editor string
