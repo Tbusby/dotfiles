04298f961b95a678d392d9a6dd7053e31e50000e
Tomas Busby <tomas.busby@gmail.com>
2015-05-21 14:30:07 +0100

    added screenrc

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 ###### Variables
 dir=~/dotfiles		# dotfiles dir
 olddir=~/old-dotfiles	# old dotfiles backup dir
-files="bashrc ssh vimrc vim zshrc zsh"	# list of files/folders to symlink in home
+files="bashrc screenrc ssh vimrc vim zshrc zsh"	# list of files/folders to symlink in home
 
 ######
 
