09b9092d771014ccda0a85bbec829475fd1936e4
Tomas Busby <tomas.busby@gmail.com>
2026-07-25 13:24:36 +0100

    fixed deleted zsh cnfig

---
+++ zsh/.config/zsh/prompt.zsh
@@ -0,0 +1,7 @@
+# ~/.config/zsh/prompt.zsh
+
+# Prevent Python virtualenv from polluting the prompt
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+
+eval "$(oh-my-posh init zsh --config ~/.config/zsh/theme.omp.toml)"
+
